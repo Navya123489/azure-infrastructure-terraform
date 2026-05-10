3659cba6020d4f3efe22fcf7706d32838394a2a7
Navya123489 <navyakreddy0304@gmail.com>
2026-05-10 15:30:04 +0100

    Add Network Security Group with SSH rule

--- main.tf
+++ main.tf
@@ -52,3 +52,23 @@ resource "azurerm_subnet" "main" {
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+resource "azurerm_network_security_group" "main" {
+  name                = "nsg-myproject-dev"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    name                      = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"        
+  }
+  tags = {
+    environment = var.environment
+    managed_by  = "terraform"
+  }
+}
