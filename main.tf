dbb3cb40dadb02dccd7e83ce1350d5a9f2d89938
Navya123489 <navyakreddy0304@gmail.com>
2026-05-10 21:47:33 +0100

    Add storage account

--- main.tf
+++ main.tf
@@ -91,9 +91,11 @@ resource "azurerm_storage_account" "main" {
   resource_group_name      = azurerm_resource_group.main.name
   location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
   
   tags = {
-    environment = "staging"
+    environment = var.environment
+
+    managed_by  = "terraform"
   }
 }
