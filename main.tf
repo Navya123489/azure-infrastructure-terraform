3b019e53e2ce10f7c86c5fb734ae00bec283ff5e
Navya123489 <navyakreddy0304@gmail.com>
2026-05-10 15:03:49 +0100

    Add subnet resource

--- main.tf
+++ main.tf
@@ -46,3 +46,9 @@ resource "azurerm_virtual_network" "main" {
     managed_by  = "terraform"
   }
 }
+resource "azurerm_subnet" "main" {
+  name                 = "subnet-myproject-dev"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
