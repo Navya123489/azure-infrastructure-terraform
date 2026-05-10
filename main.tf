8be0523f6577c320df5acda7e58088f4aa3681a0
Navya123489 <navyakreddy0304@gmail.com>
2026-05-10 21:00:15 +0100

    Add HTTP and HTTPS inbound rules to NSG

--- main.tf
+++ main.tf
@@ -54,6 +54,29 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"        
   }
+  
+security_rule {
+    name                       = "allow-http"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+security_rule {
+    name                       = "allow-https"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
   tags = {
     environment = var.environment
     managed_by  = "terraform"
